eac0633e0e7621ac92fe0ae4a33a75e0504b1d36
Rogério Vilela <rogerio.lopesvll@gmail.com>
2022-02-20 22:10:29 -0300

    Initial commit

---
+++ src/main/java/com/blog/eternoaprendiz/EternoaprendizApplication.java
@@ -0,0 +1,13 @@
+package com.blog.eternoaprendiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternoaprendizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternoaprendizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/eternoaprendiz/EternoaprendizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.eternoaprendiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternoaprendizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
